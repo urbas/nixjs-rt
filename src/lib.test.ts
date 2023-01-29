6196d1bceb5bec809076375872c9d9fdc720b297
Matej Urbas <matej.urbas@gmail.com>
2023-01-29 20:28:55 +0000

    arithmetic operations

--- src/lib.test.ts
+++ src/lib.test.ts
@@ -1,7 +1,47 @@
 import { expect, test } from "@jest/globals";
-import { add, NixInt } from "./lib";
+import nixrt, { NixInt } from "./lib";
 
-test("adds 1 + 2 to equal 3", () => {
-  const result = add(new NixInt(1), new NixInt(2)) as NixInt;
+test("negative NixInt(1) equals to NixInt(-1)", () => {
+  const result = nixrt.neg(new NixInt(1)) as NixInt;
+  expect(result.value).toBe(-1);
+});
+
+test("adds two NixInts", () => {
+  const result = nixrt.add(new NixInt(1), new NixInt(2)) as NixInt;
   expect(result.value).toBe(3);
+});
+
+test("adds two floats", () => {
+  const result = nixrt.add(1.0, 2.0) as number;
+  expect(result).toBe(3);
+});
+
+test("subtracts two NixInts", () => {
+  const result = nixrt.sub(new NixInt(1), new NixInt(2)) as NixInt;
+  expect(result.value).toBe(-1);
+});
+
+test("subtracts two floats", () => {
+  const result = nixrt.sub(1.0, 2.0) as number;
+  expect(result).toBe(-1);
+});
+
+test("multiplies two NixInts", () => {
+  const result = nixrt.mul(new NixInt(2), new NixInt(3)) as NixInt;
+  expect(result.value).toBe(6);
+});
+
+test("multiplies two floats", () => {
+  const result = nixrt.mul(2.0, 3.5) as number;
+  expect(result).toBe(7);
+});
+
+test("divides two NixInts", () => {
+  const result = nixrt.div(new NixInt(5), new NixInt(2)) as NixInt;
+  expect(result.value).toBe(2);
+});
+
+test("divides two floats", () => {
+  const result = nixrt.div(5.0, 2) as number;
+  expect(result).toBe(2.5);
 });
--- src/lib.ts
+++ src/lib.ts
@@ -6,17 +6,55 @@ export class NixInt {
   }
 }
 
-// This is the implementation of the binary `+` operator in Nix.
+// Arithmetic:
+export function neg(operand: any): any {
+  if (operand instanceof NixInt) {
+    return new NixInt(-operand.value);
+  }
+  return -operand;
+}
+
 export function add(lhs: any, rhs: any): any {
   if (lhs instanceof NixInt) {
     if (rhs instanceof NixInt) {
       return new NixInt(lhs.value + rhs.value);
     }
   }
-  return lhs;
+  return lhs + rhs;
+}
+
+export function sub(lhs: any, rhs: any): any {
+  if (lhs instanceof NixInt) {
+    if (rhs instanceof NixInt) {
+      return new NixInt(lhs.value - rhs.value);
+    }
+  }
+  return lhs - rhs;
+}
+
+export function mul(lhs: any, rhs: any): any {
+  if (lhs instanceof NixInt) {
+    if (rhs instanceof NixInt) {
+      return new NixInt(lhs.value * rhs.value);
+    }
+  }
+  return lhs * rhs;
+}
+
+export function div(lhs: any, rhs: any): any {
+  if (lhs instanceof NixInt) {
+    if (rhs instanceof NixInt) {
+      return new NixInt(Math.floor(lhs.value / rhs.value));
+    }
+  }
+  return lhs / rhs;
 }
 
 export default {
-  add: add,
-  NixInt: NixInt,
+  add,
+  div,
+  mul,
+  neg,
+  NixInt,
+  sub,
 };
