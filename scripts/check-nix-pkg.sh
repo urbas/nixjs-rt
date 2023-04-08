c20d7a128a848a43569a00857c817571b189bc5a
Matej Urbas <matej.urbas@gmail.com>
2023-04-08 18:56:56 +0100

    check and cross emojis in check scripts

--- scripts/check-nix-pkg.sh
+++ scripts/check-nix-pkg.sh
@@ -8,8 +8,8 @@ nix build --out-link $outLink
 function expectFile() {
   local theFile=$1
   [ -f "$theFile" ] \
-    && ( echo "The file '$theFile' exists." ) \
-    || ( echo "Could not find the file '$theFile'." && exit 1 )
+    && ( echo "✅ The file '$theFile' exists." ) \
+    || ( echo "❌ Could not find the file '$theFile'." && exit 1 )
 }
 
 expectFile $outLink/lib/node_modules/nixrt/src/lib.ts
--- scripts/check-npm-deps-hash.sh
+++ scripts/check-npm-deps-hash.sh
@@ -3,5 +3,5 @@
 expectedHash=$(prefetch-npm-deps package-lock.json 2> /dev/null)
 
 grep -q --fixed-string "$expectedHash" pkg.nix \
-  && ( echo "The 'npmDepsHash' attribute in 'pkg.nix' is up-to-date." ) \
-  || ( echo "The 'npmDepsHash' attribute in 'pkg.nix' is not up-to-date (expected: '$expectedHash')." && exit 1 )
+  && ( echo "✅ The 'npmDepsHash' attribute in 'pkg.nix' is up-to-date." ) \
+  || ( echo "❌ The 'npmDepsHash' attribute in 'pkg.nix' is not up-to-date (expected: '$expectedHash')." && exit 1 )
