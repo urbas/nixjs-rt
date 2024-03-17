3e4a5be897673123a18d338a789b98dfe7648287
arduano <leonid.shchurov@gmail.com>
2024-03-17 16:59:00 +1100

    Make sure the files are copied using package.json

--- pkg.nix
+++ pkg.nix
@@ -9,8 +9,4 @@ buildNpmPackage rec {
   name = "nixjs-rt";
   src = self;
   npmDepsHash = "sha256-Irgqy5OTXg6vAdyOvRuYPpwlwYoNLdGh4Ps4U9lKiSc=";
-
-  postInstall = ''
-    cp -r dist $out/lib/node_modules/${name}
-  '';
 }
