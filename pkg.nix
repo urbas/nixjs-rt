3d530c713422f816f1c2d36eb5c245d25b8c26e1
arduano <leonid.shchurov@gmail.com>
2024-03-17 16:51:58 +1100

    Fixed up package building

--- pkg.nix
+++ pkg.nix
@@ -4,8 +4,13 @@ let
 
   inherit (pkgs) buildNpmPackage;
 
-in buildNpmPackage {
+in
+buildNpmPackage rec {
   name = "nixjs-rt";
   src = self;
   npmDepsHash = "sha256-Irgqy5OTXg6vAdyOvRuYPpwlwYoNLdGh4Ps4U9lKiSc=";
+
+  postInstall = ''
+    cp -r dist $out/lib/node_modules/${name}
+  '';
 }
